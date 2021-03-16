7a716e002f137d657e2215555f283db0021785f9
EricMinh <32127944+EricMinh@users.noreply.github.com>
2021-03-16 01:45:24 -0700

    Update mainPayment.js

--- scripts/mainPayment.js
+++ scripts/mainPayment.js
@@ -2,7 +2,23 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-payment="form"]';
     var App = window.App;
+
     var FormHandler = App.FormHandler;
     var formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler();
+
+    formHandler.addSubmitHandler(function (data) {
+        $(".modalText").text("Thank you for your payment, " + data.title + " " + data.username);
+    });
+
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
 })(window);
